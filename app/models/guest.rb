d0c95a2961e44d70a941544ead08d6b074cea0b3
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2017-07-22 15:14:38 +0800

    Wire up list and search in Table Seating page

---
+++ app/models/guest.rb
@@ -0,0 +1,18 @@
+class Guest
+  include Virtus.model
+
+  attribute :name, String
+  attribute :table, Integer
+
+  def self.fetch
+    session = GoogleDrive::Session.from_service_account_key(Rails.root.join("config", "service-account.json"))
+    spreadsheet = session.spreadsheet_by_key(Settings.google_sheets.guest_list)
+    guests = Rails.cache.fetch([Rails.env, spreadsheet.id, spreadsheet.version]) do
+      worksheet = spreadsheet.worksheets[0]
+      guests = worksheet.rows.drop(1).collect do |row|
+        Guest.new(name: row[0], table: row[1])
+      end
+      guests.sort_by { |g| g.name }
+    end
+  end
+end
